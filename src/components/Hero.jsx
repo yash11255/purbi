f05045bbdb390d6779c12f37bfc0a1e8e25be07a
testbench4 <yashsingh1125@gmail.com>
2026-01-12 09:28:58 +0530

    bannere updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,13 +8,13 @@ function Hero() {
       <div className="grid w-full">
         {/* Desktop banner */}
         <img
-          src="/banner1.1.jpg"
+          src="/Untitled-3 banner 1.3.jpg"
           alt="Hero banner desktop"
           className="hidden md:block col-start-1 row-start-1 w-full h-auto object-cover object-center transition-opacity duration-700"
         />
         {/* Mobile banner */}
         <img
-          src="/WhatsApp Image 2026-01-09 at 4.22.59 PM.jpg"
+          src="/WhatsApp Image 2026-01-09 at 4.22.59 PM (1).jpg"
           alt="Hero banner mobile"
           className="block md:hidden col-start-1 row-start-1 w-full h-auto object-cover object-center transition-opacity duration-700"
         />
